c6d8d431d997db2fb77102f54a968483266a9359
mpiranha <chukwubuezeokeke@gmail.com>
2021-03-01 03:14:54 +0100

    major page update

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -2,4 +2,11 @@
     $('.collapse-icon').on('click', function () {
         $('.expandable-sidebar').toggleClass('shrinked');
     });
+
+    $('#action-btn').on('click', function () {
+        $('.action-dropdown').toggleClass('visible-block');
+       
+    });
+    $('[data-toggle="tooltip"]').tooltip();
+
 })();
