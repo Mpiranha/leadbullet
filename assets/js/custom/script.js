89fd8d2690a41d4faa97800dabd09d5b657e0ec9
mpiranha <chukwubuezeokeke@gmail.com>
2021-10-19 14:25:27 +0100

    update to search leads

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -5,8 +5,13 @@
 
     $('#action-btn').on('click', function () {
         $('.action-dropdown').toggleClass('visible-block');
-       
+
     });
     $('[data-toggle="tooltip"]').tooltip();
+
+    var star_rating_width = $('.fill-ratings span').width();
+    // Sets the container of the ratings to span width
+    // thus the percentages in mobile will never be wrong
+    $('.star-ratings').width(star_rating_width);
 
 })();
